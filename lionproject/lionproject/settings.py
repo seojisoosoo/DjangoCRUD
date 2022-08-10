38c48c24ac66d3cbebfecaf7d1c68b9690188a18
Seojisoo20191941 <amy0480@naver.com>
2022-08-10 23:17:42 +0900

    please finish

--- lionproject/lionproject/settings.py
+++ lionproject/lionproject/settings.py
@@ -23,14 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j49xs3!t7gh2cd0_!stcat+$y_joo$#e$=0))+vx#d&_ks1x1q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = '*'
+# ALLOWED_HOSTS = '*'
 
 # 헤로쿠
-# DEBUG = False
-# ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
